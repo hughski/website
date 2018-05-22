befec98a245531f6d4c049b58b506c8cb5abb40f
Richard Hughes <richard@hughsie.com>
2018-05-22 11:30:29 +0100

    Remove legacy file

--- upload.sh
+++
@@ -1,6 +0,0 @@
-#scp -r -P 3784 * hughsiec@hughski.com:~/public_html/hughski
-scp -P 3784 *.html hughsiec@hughski.com:~/public_html/hughski
-#scp -P 3784 css/*.css hughsiec@hughski.com:~/public_html/hughski/css
-#scp -P 3784 img/* hughsiec@hughski.com:~/public_html/hughski/img
-#scp -r -P 3784 assets hughsiec@hughski.com:~/public_html/hughski/assets
-#scp -P 3784 *.php hughsiec@hughski.com:~/public_html/hughski
